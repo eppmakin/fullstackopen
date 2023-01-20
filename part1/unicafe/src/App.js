3dd287e2c9d14757b7ca36ffbed04a3ea623fe49
eppmakin <eeli.makinen98@gmail.com>
2023-01-20 16:22:36 +0200

    part 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 const App = () => {
   const header = "give feedback"
+  const header2 = "statistics"
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -14,11 +15,31 @@ const Header = (props) => {
     </div>
   )
 }
+
+
   return (
-    <div>
-      <Header header={header}/>
+    <><div>
+      <Header header={header} />
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
     </div>
+    <div>
+      <Header header={header2} />
+      good {good}
+      <br />
+      neutral {neutral}
+      <br />
+      bad {bad}
+    </div></>
   )
 }
+
 
 export default App
