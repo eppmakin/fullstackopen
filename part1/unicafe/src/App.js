788db1da08fe6047e78f88919f14bef1de1a71f7
eppmakin <eeli.makinen98@gmail.com>
2023-01-21 12:39:52 +0200

    part 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,23 +5,28 @@ const Statistics = (props) => {
   const { good, neutral, bad } = props;
   const all = good + neutral + bad
   const average = (good - bad) / all // same as before just written better
-  const positive = (good / all) * 100
+  const positive = (good / all) * 100 + "%"
   return (    
-    <>
-    <div>
-      <StatisticLine text="good"     value = {good}     />
-      <StatisticLine text="neutral"  value = {neutral}  />
-      <StatisticLine text="bad"      value = {bad}      />
-      <StatisticLine text="average"  value = {average}  />
-      <StatisticLine text="positive" value = {positive} />
-    </div>
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good"     value = {good}     />
+        <StatisticLine text="neutral"  value = {neutral}  />
+        <StatisticLine text="bad"      value = {bad}      />
+        <StatisticLine text="all"      value = {all}      />
+        <StatisticLine text="average"  value = {average}  />
+        <StatisticLine text="positive" value = {positive} />
+      </tbody>
+    </table>
   )
 }
 // Displaying a single statistics eg. the average score
 const StatisticLine = ({text, value}) => {
   return (
-    <div>{text} {value}</div>
+    // HTML table
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 // Defining the buttons used for submitting feedback
